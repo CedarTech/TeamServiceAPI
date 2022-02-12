13eef9ec57566026f6aca81bb24743468d8e3ca3
Cedar Tech <cedartechservice@gmail.com>
2022-02-11 22:44:03 -0500

    Added team DB lazy load

---
+++ src/TeamServiceAPI/Migrations/20220211182451_Updated Team model.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TeamServiceAPI.Migrations
+{
+    public partial class UpdatedTeammodel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
